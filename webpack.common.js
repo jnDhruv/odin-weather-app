9a7e8e07857c4696f08f24e4d1e78ac61a249daa
Dhruv <dhruvjain.dev@gmail.com>
2025-05-07 20:32:19 +0530

    Install html-loader

--- webpack.common.js
+++ webpack.common.js
@@ -19,18 +19,14 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
-      // -- To load images in HTML ref
-      //    - run `npm install -D html-loader`
-      // {
-      //   test: /\.html$/i,
-      //   loader: "html-loader",
-      // },
-
-      // -- To load images through JS
-      // {
-      //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      //   type: "asset/resource",
-      // },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
